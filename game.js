7bff7a9dd5026ce2be426f76f84400607e9290a8
Caitlyn Nelson <cxknelson@hotmail.com>
2026-05-20 13:52:31 -0600

    Update game.js

--- game.js
+++ game.js
@@ -1,16 +1,19 @@
 let inventory = [];
+let dialogueIndex = 0;
 
 const story = document.getElementById("story");
 const choices = document.getElementById("choices");
 const inventoryList = document.getElementById("inventory");
+const npcDialogue = document.getElementById("npcDialogue");
 
 function startGame() {
   story.innerHTML =
     "You enter the abandoned library. A silver key rests on the desk.";
 
   choices.innerHTML = `
-    <button onclick="takeKey()">Take Key</button>
-    <button onclick="leaveRoom()">Leave Room</button>
+  <button onclick="takeKey()">Take Key</button>
+  <button onclick="talkToCaretaker()">Talk to Caretaker</button>
+  <button onclick="leaveRoom()">Leave Room</button>
   `;
 }
 
@@ -30,6 +33,34 @@ function leaveRoom() {
     "The hallway is dark and silent...";
     
   choices.innerHTML = "";
+}
+
+function talkToCaretaker() {
+
+  if (inventory.includes("Silver Key")) {
+
+    npcDialogue.innerHTML = `
+      <p>
+        <strong>${caretaker.name}:</strong>
+        "I see you've found the Silver Key..."
+      </p>
+    `;
+
+    return;
+  }
+
+  npcDialogue.innerHTML = `
+    <p>
+      <strong>${caretaker.name}:</strong>
+      "${caretaker.dialogue[dialogueIndex]}"
+    </p>
+  `;
+
+  dialogueIndex++;
+
+  if (dialogueIndex >= caretaker.dialogue.length) {
+    dialogueIndex = 0;
+  }
 }
 
 function updateInventory() {
